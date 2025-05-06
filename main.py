88625478695373249825105afa95b1dfafc6539c
Samuel630663 <samuelecheverrysaenz2020@gmail.com>
2025-05-05 20:31:15 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import discord
+import random
+import os
+import requests
 from discord.ext import commands
 
 intents = discord.Intents.default()
@@ -50,6 +53,37 @@ async def add(ctx, a: int, b: int):
 async def slap(ctx, members: commands.Greedy[discord.Member], *, reason='no reason'):
     slapped = ", ".join(x.name for x in members)
     await ctx.send(f'{slapped} just got slapped for {reason}')
+    
+@bot.command()
+async def mem(ctx):
+    with open('imagenes/mem1.jpg', 'rb') as f:
+        # ¡Vamos a almacenar el archivo de la biblioteca Discord convertido en esta variable!
+        picture = discord.File(f)
+    # A continuación, podemos enviar este archivo como parámetro.
+    await ctx.send(file=picture)
+    
+@bot.command()
+async def meme_aleatori(ctx):
+    
+    nombre_imagen = random.choice(os.listdir('imagenes'))
+    
+    with open(f'imagenes/{nombre_imagen}', 'rb') as f:
+        # ¡Vamos a almacenar el archivo de la biblioteca Discord convertido en esta variable!
+        picture = discord.File(f)
+    # A continuación, podemos enviar este archivo como parámetro.
+    await ctx.send(file=picture)
+
+def get_perro_imagen():    
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('perro')
+async def imagen_perro(ctx):
+    
+    image_url = get_perro_imagen()
+    await ctx.send(image_url)
 
 
 token = ''
